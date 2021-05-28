744e20ad7043a792957672373959143484c9f130
Corrinne W <corrinneworden@gmail.com>
2021-05-27 23:03:26 -0400

    registered service worker in index.html. made changes to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,63 +1,48 @@
-  const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/index.js',
-    '/db.js',
-    '/styles.css',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/db.js',
+  '/styles.css',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
-  
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
-    );
-  });
-  
-  // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-    event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
-            })
-          );
-        })
-        .then(() => self.clients.claim())
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
-          }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
+
+const STATIC_CACHE = 'static-cache';
+const DATA_CACHE = 'data-cache';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE).then(() => {
+        return caches.open(DATA_CACHE).then((cache) => {
+          return fetch(event.request).then((response) => {
+
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone())
+            }
+            return response;
           });
-        })
-      );
-    }
-  });
-  
+        });
+      })
+    );
+    return;
+  }
+  event.respondWith(
+    fetch (event.request).catch(function() {
+      return caches.match(event.request).then(response => {
+        if (response) return response;
+        if (event.request.headers.get('accept').includes('text/html')) return caches.match('/');
+      })
+    })
+  )
+});
